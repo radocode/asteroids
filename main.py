18bd9c6dbc42e316b8e2cecea08c8fa24ad5c36e
radocode <francisco.gonzalez.lorca@gmail.com>
2025-02-08 23:29:46 -0300

    rotato

--- main.py
+++ main.py
@@ -3,20 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    cl = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = cl.tick(60)/1000
+        player.update(dt)
 
 
 if __name__ == "__main__":
