5e77f0acf79df55e0183f623ddcbcedb8b949d10
radocode <francisco.gonzalez.lorca@gmail.com>
2025-02-13 06:16:13 -0300

    we have some asteroids

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    cl = pygame.time.Clock()
-    dt = 0
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    updatable.add(player)
-    drawable.add(player)
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return        
-            
+                return
+
         updatable.update(dt)
 
         screen.fill("black")
 
-        for dr in drawable:
-            dr.draw(screen)
-    
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        dt = cl.tick(60)/1000
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
